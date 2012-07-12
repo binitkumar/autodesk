e7d035453572e56c9f08f86ce4e952405057081a
Henry Owens <owens.henry@googlemail.com>
2012-07-12 16:28:35 +0100

    Added first_name and last_name to user model and migration

--- db/migrate/20120712150741_devise_create_users.rb
+++ db/migrate/20120712150741_devise_create_users.rb
@@ -42,5 +42,10 @@ class DeviseCreateUsers < ActiveRecord::Migration
     # add_index :users, :confirmation_token,   :unique => true
     # add_index :users, :unlock_token,         :unique => true
     # add_index :users, :authentication_token, :unique => true
+    
+    # Custom fields
+    t.string :first_name
+    t.string :last_name
+    
   end
 end
