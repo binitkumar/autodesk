23b7f202dcabdf01f7185a8657ba41ea980b1e62
Henry Owens <owens.henry@googlemail.com>
2012-07-12 16:31:04 +0100

    Fixed typo

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ActiveRecord::Base
          :omniauthable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me
+  attr_accessible :email, :password, :password_confirmation, :remember_me,
                   :first_name, :last_name
   # attr_accessible :title, :body
 end
