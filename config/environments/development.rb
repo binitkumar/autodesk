d283a91c238b2e2db3042eef3df71b0739b6b51f
Henry Owens <owens.henry@googlemail.com>
2012-07-12 16:02:58 +0100

    Added default url for devise

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,4 +34,7 @@ Autobase::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
+  
+  # Create default url options for Devise
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 end
