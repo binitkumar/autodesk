33421828bedfecea1d1fc7d00c17b090b49f7247
Henry Owens <owens.henry@googlemail.com>
2012-07-13 11:27:36 +0100

    Deleted Event_comments model

--- app/models/event_comment.rb
+++
@@ -1,3 +0,0 @@
-class EventComment < ActiveRecord::Base
-  attr_accessible :user_id, :event_id, :value
-end
--- test/unit/event_comment_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class EventCommentTest < ActiveSupport::TestCase
-  def test_should_be_valid
-    assert EventComment.new.valid?
-  end
-end
