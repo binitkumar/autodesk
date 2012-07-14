9c817bcdc8a1fabba645eef9996e86b49d72932e
Henry Owens <owens.henry@googlemail.com>
2012-07-14 23:12:11 +0100

    Added relationships to Vehicle model

--- app/models/vehicle.rb
+++ app/models/vehicle.rb
@@ -1,3 +1,24 @@
 class Vehicle < ActiveRecord::Base
   attr_accessible :make_id, :model_id, :trim_id
+  
+  belongs_to :make
+  belongs_to :model
+  belongs_to :trim
+  
+  has_many :mileage_readings
+  
+  has_many :registration_mark_vehicles
+  has_many :registration_marks, :through => :registration_mark_vehicles
+  
+  has_many :feature_vehicles
+  has_many :features, :through => :feature_vehicles
+  
+  has_many :sale_vehicles
+  has_many :sales, :through => :sale_vehicles
+  
+  has_many :quote_vehicles
+  has_many :quotes, :through => :quote_vehicles
+  
+  has_many :purchase_vehicles
+  has_many :purchases, :through => :purchase_vehicles
 end
