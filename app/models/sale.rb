a66d83d1d29d14337a4cf0e709d15dc44449d9c0
Henry Owens <owens.henry@googlemail.com>
2012-07-15 12:08:46 +0100

    Added relationships to Sale model

--- app/models/sale.rb
+++ app/models/sale.rb
@@ -1,3 +1,23 @@
 class Sale < ActiveRecord::Base
   attr_accessible :dealer_id, :customer_id, :date, :sale_type_id
+  
+  belongs_to :dealer
+  belongs_to :customer
+  belongs_to :sale_type
+  
+  has_many :product_sales
+  has_many :products, :through => :product_sales
+  
+  has_many :sale_users
+  has_many :users, :through => :sale_users
+  
+  has_many :sale_vehicles
+  has_many :vehicles, :through => :sale_vehicles
+  
+  has_many :financial_transaction_sales
+  has_many :financial_transactions, :through => :financial_transaction_sales
+  
+  has_many :funding_plan_sales
+  has_many :funding_plans, :through => :funding_plan_sales
+  
 end
