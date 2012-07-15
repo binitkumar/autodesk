0ae248e2e2763033f679d23ea532277e4f49a27f
Henry Owens <owens.henry@googlemail.com>
2012-07-15 12:37:15 +0100

    Added relationships to Financial_transaction model

--- app/models/financial_transaction.rb
+++ app/models/financial_transaction.rb
@@ -1,3 +1,12 @@
 class FinancialTransaction < ActiveRecord::Base
   attr_accessible :dealer_id, :financial_transaction_type_id, :financial_transaction_method_id, :currency_id, :value, :time, :debit
+  
+  belongs_to :dealer
+  belongs_to :financial_transaction_type
+  belongs_to :financial_transaction_method
+  belongs_to :currency
+  
+  has_many :financial_transaction_sales
+  has_many :sales, :through => :financial_transaction_sales
+
 end
