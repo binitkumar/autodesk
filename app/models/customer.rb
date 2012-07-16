4707b39a2b9c1819ad38ab4d18c5c71c543c9309
Henry Owens <owens.henry@googlemail.com>
2012-07-16 09:33:55 +0100

    Added relationships to Customer, Event and Event_user models

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,3 +1,28 @@
 class Customer < ActiveRecord::Base
   attr_accessible :customer_type_id
+  
+  belongs_to :customer_type
+  
+  has_many :quotes
+  has_many :sales
+  has_many :employments
+  
+  has_many :customer_events
+  has_many :events, :through => :customer_events
+  
+  has_many :customer_emails
+  has_many :emails, :through => :customer_emails
+  
+  has_many :customer_contact_numbers
+  has_many :contact_numbers, :through => :customer_contact_numbers
+  
+  has_many :customer_dealers
+  has_many :dealers, :through => :customer_dealers
+  
+  has_many :customer_addresses
+  has_many :addresses, :through => :customer_addresses
+  
+  has_many :customer_features
+  has_many :features, :through => :customer_features
+  
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,12 @@
 class Event < ActiveRecord::Base
   attr_accessible :time, :dealer_id
+  
+  belongs_to :dealer
+  
+  has_many :event_users
+  has_many :users, :through => :event_users
+  
+  has_many :customer_events
+  has_many :customers, :through => :customer_events
+  
 end
