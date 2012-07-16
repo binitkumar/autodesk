e0de6bbee72fbc9a50df34a23dba099fcf8e3270
Henry Owens <owens.henry@googlemail.com>
2012-07-16 10:29:53 +0100

    Added relationships to Qualification_requirement and Qualification_requirement_volume_bonus_plan models

--- app/models/qualification_requirement.rb
+++ app/models/qualification_requirement.rb
@@ -1,3 +1,9 @@
 class QualificationRequirement < ActiveRecord::Base
   attr_accessible :value, :metric_id
+  
+  belongs_to :metric
+  
+  has_many :qualification_requirement_volume_bonus_plans
+  has_many :qualification_requirements, :through => :qualification_requirement_volume_bonus_plans
+  
 end
--- app/models/qualification_requirement_volume_bonus_plan.rb
+++ app/models/qualification_requirement_volume_bonus_plan.rb
@@ -1,3 +1,7 @@
 class QualificationRequirementVolumeBonusPlan < ActiveRecord::Base
   attr_accessible :qualification_requirement_id, :volume_bonus_plan_id
+  
+  belongs_to :qualification_requirement
+  belongs_to :volume_bonus_plan
+  
 end
