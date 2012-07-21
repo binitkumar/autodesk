867271c43013c96070b3d47417245fc2983d28cb
Henry Owens <owens.henry@googlemail.com>
2012-07-21 21:41:19 +0100

    Added relationships to appraisal and valuation models

--- app/models/appraisal.rb
+++ app/models/appraisal.rb
@@ -1,3 +1,13 @@
 class Appraisal < ActiveRecord::Base
   attr_accessible :dealer_id, :vehicle_id, :time
+  
+  belongs_to :dealer
+  belongs_to :vehicle
+  
+  has_many :appraisal_appraisal_points
+  has_many :appraisal_points, :through => :appraisal_appraisal_points
+  
+  has_many :appraisal_users
+  has_many :users, :through => :appraisal_users
+  
 end
