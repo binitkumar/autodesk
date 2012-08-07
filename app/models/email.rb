89bed92b81eda5fa4a3b53b3089e47486510267e
Henry Owens <owens.henry@googlemail.com>
2012-08-07 22:15:23 +0100

    Changed user email relationship to work with Devise

--- app/models/email.rb
+++ app/models/email.rb
@@ -4,9 +4,6 @@ class Email < ActiveRecord::Base
   has_many :dealer_emails
   has_many :dealers, :through => :dealer_emails
   
-  has_many :email_users
-  has_many :users, :through => :email_users
-  
   has_many :customer_emails
   has_many :customers, :through => :customer_emails
   
--- app/models/email_user.rb
+++
@@ -1,7 +0,0 @@
-class EmailUser < ActiveRecord::Base
-  attr_accessible :user_id, :email_id, :primary
-  
-  belongs_to :user
-  belongs_to :email
-  
-end
