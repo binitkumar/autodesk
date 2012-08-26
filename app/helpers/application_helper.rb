415ac3a821d7944771c4232fde491ed7635f915c
Henry Owens <owens.henry@googlemail.com>
2012-08-26 12:16:50 +0100

    Additional form updates and fixes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,7 +10,7 @@ module ApplicationHelper
   end
   
   def add_contact_number_fields(contact_number_types, object_form_builder, actions_visible)
-    feature_types.length.times {object_form_builder.object.features.build}
+    contact_number_types.length.times {object_form_builder.object.contact_numbers.build}
     i = -1
     object_form_builder.fields_for :contact_numbers do |contact_numbers_builder|
       i = i + 1
