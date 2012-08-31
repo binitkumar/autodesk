5a85edf8657c4f6100e0d94c9adcf7556118f94c
Henry Owens <owens.henry@googlemail.com>
2012-08-31 10:11:25 +0100

    Updates to add_fixed_fields to allow building of belongs_to objects

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,15 @@
 module ApplicationHelper
   
-  def add_fixed_fields(type, fields, object_form_builder, actions_visible)
-    fields.length.times {object_form_builder.object.send(type.to_sym).build}
+  def add_fixed_fields(type, fields, object_form_builder, actions_visible, belongs_to)
+    if belongs_to
+      object_form_builder.object.send("build_#{type}")
+    else
+      fields.length.times {object_form_builder.object.send(type.to_sym).build}
+    end
     i = -1
-    object_form_builder.fields_for type do |y|
+    object_form_builder.fields_for type do |f|
       i = i + 1
-      render :partial => type + "/fixed_" + type.singularize, :locals => {type.singularize.to_sym => y, ('fixed_' + type.singularize + '_type').to_sym => fields[i], :form_actions_visible => actions_visible}
+      render :partial => type.pluralize + "/fixed_" + type.singularize, :locals => {:f => f, ('fixed_' + type.singularize + '_type').to_sym => fields[i], :form_actions_visible => actions_visible}
     end
   end
   
