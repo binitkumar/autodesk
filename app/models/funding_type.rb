370bb17ee5be21c7b44e4402c7a7b2ec1df78546
Henry Owens <owens.henry@googlemail.com>
2012-09-27 12:47:22 +0100

    Add suppliers attribute to funding_type model

--- app/models/funding_type.rb
+++ app/models/funding_type.rb
@@ -2,6 +2,8 @@ class FundingType < ActiveRecord::Base
   attr_accessible :value
   
   has_many :dealer_funding_types
+  
   has_many :funding_plans
+  has_many :suppliers, :through => :funding_plans
   
 end
