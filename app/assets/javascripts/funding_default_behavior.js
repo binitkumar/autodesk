3789a295820c76d9ea63ea9d9e9af452b5437eae
Henry Owens <owens.henry@googlemail.com>
2012-10-11 11:31:46 +0100

    Subtract values of other funding methods from cash amount

--- app/assets/javascripts/funding_default_behavior.js
+++ app/assets/javascripts/funding_default_behavior.js
@@ -1,3 +1,16 @@
+function sumPriceFundingFields () {
+	var sum = 0;
+	$('.price-contributor').each(function(){
+		// Add up the values of the price contributor fields, stripping out any non-numeric elements
+		sum += parseFloat( ('0' + $(this).val()).replace(/[^0-9-\.]/g, ''), 10 );
+	});
+	$('.price-subtractor').each(function(){
+		// Subtract the values of the price subtractor fields from the sum, stripping out any non-numeric elements
+		sum = sum - parseFloat( ('0' + $(this).val()).replace(/[^0-9-\.]/g, ''), 10 );
+	});
+	$('.funding-method-container:not([data-timestamp])').find('.funding-plan-amount').val(sum);
+};
+
 function fundingDefaultBehavior () {
 	// Set the funding type to cash and disable the Funding Type selector
 	$('#funding_type_selector option').filter(function() {
@@ -10,17 +23,16 @@ function fundingDefaultBehavior () {
 	$('#funding_supplier_selector').attr('disabled', true).trigger("liszt:updated");
 	$('#funding_supplier_selector').parent().parent().hide();
 
-	// Disable the Funding Amount text box
-	$('.funding-method-container:not([data-timestamp])').find('.funding-plan-amount').attr('disabled', true);
+	// Disable the Funding Amount text box, and remove it from the sums
+	$('.funding-method-container:not([data-timestamp])').find('.funding-plan-amount').attr('disabled', true).removeClass('price-subtractor');
 
 	// Bind to the change event of the price-contributor fields
 	$('.price-contributor').change(function() {
-		// If the initial funding plan box has been manually updated, don't do anything
-		var sum = 0;
-		$('.price-contributor').each(function(){
-			// Add up the values of the price contributor fields, stripping out any non-numeric elements
-			sum += parseFloat( ('0' + $(this).val()).replace(/[^0-9-\.]/g, ''), 10 );
-		});
-		$('.funding-method-container:not([data-timestamp])').find('.funding-plan-amount').val(sum);
+		sumPriceFundingFields();
+	});
+	
+	// Bind to the change event of the price-subtractor fields
+	$('.price-subtractor').change(function() {
+		sumPriceFundingFields();
 	});
 };
